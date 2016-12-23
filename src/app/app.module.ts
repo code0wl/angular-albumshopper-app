10c6bcd7f358846ee4e153a95768ca6fdfb790e7
Oz <o.lodriguez@gmail.com>
2016-12-23 10:43:44 +0100

    styles for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+import  '../../node_modules/purecss/build/pure-min.css';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
