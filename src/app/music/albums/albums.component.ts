2171caa772222c564d3a49fba7050bfef9320a0d
Oz <o.lodriguez@gmail.com>
2016-12-24 13:27:42 +0100

    Styling and dummy content

--- src/app/music/albums/albums.component.ts
+++ src/app/music/albums/albums.component.ts
@@ -14,18 +14,18 @@ export class AlbumsComponent implements OnInit {
   ngOnInit() {
     this.albums = [
       {
-        release: 'some release',
-        title: 'some title',
-        artists: [{ name: 'some artist' }],
-        genres: ['some genre'],
+        release: '1999',
+        title: 'Xscape',
+        artists: [{ name: 'Michael Jackson' }],
+        genres: ['Pop'],
         cover: 'http://illusion.scene360.com/wp-content/uploads/2014/10/computergraphics-album-covers-2014-15.jpg',
-        label: 'some label'
+        label: 'Universal'
       },
       {
-        release: 'some release',
-        title: 'some title',
+        release: '2010',
+        title: 'Art vs Science',
         artists: [{ name: 'some artist' }],
-        genres: ['some genre'],
+        genres: ['experimental'],
         label: 'some label',
         cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/602f4731226337.5646928c3633f.jpg'
       }
--- src/app/shopping-cart/shopping-list/shopping-list.component.ts
+++ src/app/shopping-cart/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Album } from './../../music/shared/models/album.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  albums: Array<Album>;
   constructor() { }
 
   ngOnInit() {
+   this.albums = [
+      {
+        release: '1999',
+        title: 'Xscape',
+        artists: [{ name: 'Michael Jackson' }],
+        genres: ['Pop'],
+        cover: 'http://illusion.scene360.com/wp-content/uploads/2014/10/computergraphics-album-covers-2014-15.jpg',
+        label: 'Universal'
+      },
+      {
+        release: '2010',
+        title: 'Art vs Science',
+        artists: [{ name: 'some artist' }],
+        genres: ['experimental'],
+        label: 'some label',
+        cover: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/602f4731226337.5646928c3633f.jpg'
+      }
+    ];
   }
-
 }
